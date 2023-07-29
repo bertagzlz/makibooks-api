4f838893b0f971fc0c40b2334ba4f07309865f71
bertagzlz <jrodenaspua@gmail.com>
2023-07-29 14:43:35 +0200

    "seventh"

--- api/app/public/php/classes/JwtHandler.php
+++ api/app/public/php/classes/JwtHandler.php
@@ -3,7 +3,7 @@ $path = dirname(__DIR__);
 //require $path."/vendor/autoload.php";
 //require '/var/task/user/api/app/public/php/vendor/autoload.php';
 //https://makibooks-api.vercel.app/api/app/public/php
-require dirname($_SERVER['PHP_SELF']).'vendor/autoload.php';
+require dirname($_SERVER['PHP_SELF']).'/vendor/autoload.php';
 
 use Firebase\JWT\JWT;
 
