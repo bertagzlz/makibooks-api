b5b3e80f028f49012b4d1d741c0846d016a5d6ff
bertagzlz <jrodenaspua@gmail.com>
2023-07-29 13:22:36 +0200

    "seventh"

--- api/app/public/php/users/authenticate.php
+++ api/app/public/php/users/authenticate.php
@@ -9,8 +9,10 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 //require __DIR__ . '../classes/JwtHandler.php';
 //require '../classes/Database.php';
 //require '../classes/JwtHandler.php';
-require __DIR__ . '../classes/Database.php';
-require __DIR__ . '../classes/JwtHandler.php';
+$path = __DIR__ . "../classes/";
+require $path."Database.php";
+// require __DIR__ . '../classes/JwtHandler.php';
+require $path."JwtHandler.php";
 
 function msg($success,$status,$message,$extra = []){
     return array_merge([
