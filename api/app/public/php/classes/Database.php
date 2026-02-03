f8ceac470e26f2fc95facb8d4f25e013bae875d0
berta <bertagzlz@gmail.com>
2026-02-03 13:21:54 +0100

    Update database connection details

--- api/app/public/php/classes/Database.php
+++ api/app/public/php/classes/Database.php
@@ -6,11 +6,15 @@ class Database{
     //private $db_host = 'awseb-e-ckdjb8rum3-stack-awsebrdsdatabase-rupg1aujpysx.cbldhoc4glmk.eu-west-3.rds.amazonaws.com';
     //private $db_name = 'ebdb'; // elastic bean
 
-    private $db_host = 'www.db4free.net'; //'mysqldb';
+    /*private $db_host = 'www.db4free.net';
     private $db_name = 'makibooks';
-
     private $db_username = 'joserod';
-    private $db_password = 'Ac640100jr';
+    private $db_password = 'Ac640100jr';*/
+    private $db_host = 'sql7.freesqldatabase.com';//'www.db4free.net';
+    private $db_name = 'sql7816069'; //'makibooks';
+    //   A L L W A Y S
+    private $db_username = 'sql7816069'; //'joserod';
+    private $db_password = 'gAQXG2K4fA'; //'Ac640100jr';
     
     public function dbConnection(){
         
